d7a291ce4d2354069925aa87f1800abe73b178f1
Andrew Cutler <Andrew.Cutler@csi.cuny.edu>
2019-04-30 15:49:00 -0400

    switched from parsing c-strings to reading ints

--- RadixSort/main.cpp
+++ RadixSort/main.cpp
@@ -1,15 +1,22 @@
 #include "iostream"
 #include "fstream"
 #include <string>
+#include <cmath>
 #include <queue>
 
 using namespace std;
 
-void main() {
-	ifstream inFile;
-	inFile.open("data.txt");
+int main(int argc, char* argv[]) {
+	//max amount of numbers to sort
 	const int NUM_OF_INTS = 10;
-	string numbers[NUM_OF_INTS];
+	int numbers[NUM_OF_INTS];
+
+	//read filename from cli
+	string filename = argv[1];
+
+	//open file
+	ifstream inFile;
+	inFile.open(filename);
 
 	//read data into array
 	for (int i = 0; i < NUM_OF_INTS; i++) {
@@ -17,25 +24,29 @@ void main() {
 	}
 
 	//create array of queues, one for each digit 0-9
-	queue<string> qArray[NUM_OF_INTS];
+	queue<int> qArray[NUM_OF_INTS];
 
-	for (int j = 4; j > 0; j--) {
+	for (int j = 0; j < 4; j++) { //sorts up to 4 digits
 		for (int i = 0; i < NUM_OF_INTS; i++) {
-			//read each digit into appropriate array
-			int value = (numbers[i][j-1] - 48);	//convert ASCII value to numeric value
-			qArray[value].push(numbers[i]);
+			int root = pow(10, j); //digit to sort by, from least to most significant
+			int digit = (numbers[i]/root) % 10; //value of that digit
+			qArray[digit].push(numbers[i]); //put number into appropriate digit queue
 		}
-		int l = 0;
-		for (int k = 0; k < NUM_OF_INTS; k++) {
-			//moving each element from queue back to array in sorted order
-			while (!qArray[k].empty()) {
-				numbers[l] = qArray[k].front(); //grab from front of queue
-				qArray[k].pop(); //remove from queue
-				l++; //increment array index
+		int l = 0; 
+		for (int k = 0; k < NUM_OF_INTS; k++) { //move sorted numbers back to numbers array
+			while (!qArray[k].empty()) { //while each queue is not empty...
+				numbers[l] = qArray[k].front(); //...move front into next slot of numbers array
+				qArray[k].pop();	//...pop from queues in order
+				l++;	//increment index of numbers array
 			}
-			//repeat for each digit (NUM_OF_INTS)
 		}
-	} //array is now sorted 
+	}	//array now sorted
 
-	return;
+	std::cout << "Sorted numbers:\n";
+	for (int i = 0; i < NUM_OF_INTS; i++) {
+		std::cout << numbers[i] << " ";
+	}
+	std::cout << "\n";
+
+	return 0;
 }
